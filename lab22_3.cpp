5233c16284164cf7ee3db2a0b7bf88ae65c9ab8f
PEEMTM <76035600+PEEMTM@users.noreply.github.com>
2021-03-12 16:18:00 +0700

    finish

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
-//Write function count() here.
+#include<iostream>
+#include<set>
+using namespace std;
+
+int count(int data[],unsigned int a){
+	set<int> myCount;
+	for(unsigned int i = 0 ; i < a ; i++){
+		myCount.insert(data[i]);
+	}
+	return myCount.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
